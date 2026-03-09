13d8c15edd9b2fea4d391f1c4642e766af4025f7
blaiselvndro-bit <blaise.lvndro@gmail.com>
2026-03-09 23:51:43 +0800

    Update bot.py

--- bot.py
+++ bot.py
@@ -327,6 +327,9 @@ async def fake_chat_loop(context, user_id):
 
             context.user_data["missed_checks"] += 1
 
+            if context.user_data["missed_checks"] == 2:
+                asyncio.create_task(live_location_loop(context, user_id))
+
             username = context.user_data.get("username")
             contacts = get_contacts(user_id)
 
@@ -403,7 +406,24 @@ async def sos_check_loop(context, user_id):
                     )
 
                 break
+# ---------------- LIVE LOCATION LOOP ----------------
 
+async def live_location_loop(context, user_id):
+
+    while context.user_data.get("sos_active") or context.user_data.get("step") == "fake_chat":
+
+        lat = context.user_data.get("last_lat")
+        lon = context.user_data.get("last_lon")
+
+        if not lat or not lon:
+            return
+
+        contacts = get_contacts(user_id)
+
+        for c in contacts:
+            await context.bot.send_location(c, lat, lon)
+
+        await asyncio.sleep(15)
 
 # ---------------- PHOTO HANDLER ----------------
 
@@ -544,6 +564,9 @@ async def location_handler(update: Update, context: ContextTypes.DEFAULT_TYPE):
 
     lat = update.message.location.latitude
     lon = update.message.location.longitude
+
+    context.user_data["last_lat"] = lat
+    context.user_data["last_lon"] = lon
 
     keyboard = InlineKeyboardMarkup([
         [
